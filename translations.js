e84d17d0de0c33b5787fe99d46f3cf52ad5fd00f
Roen <tararoneee@gmail.com>
2026-08-11 00:00:12 +0800

    First Release 1.0.9

--- translations.js
+++ translations.js
@@ -241,7 +241,7 @@ const translations = {
 'label-wedding': '結婚對戒',
         'label-set': '戒指套裝',
         'label-details': '詳細資料',
-        'detail-centerStone': '主石',
+        'detail-centreStone': '主石',
         'detail-eachDiamond': '每顆鑽石重量',
         'detail-mainSide': '主側鑽重量',
         'detail-secondarySide': '副側鑽重量',
